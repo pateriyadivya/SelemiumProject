93c5d66a53211913025e9fa9fb4d793a710b0b4d
divya pateriya <divyapateriya14@outlook.com>
2023-02-26 13:26:45 +0530

    choose the browser as per user input, should provide the input through CLI

--- pytest_data/e2e_framework/conftest.py
+++ pytest_data/e2e_framework/conftest.py
@@ -4,17 +4,27 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
+#Introducing command line options
+# Here we will expect the user to give a command line input in case they wish to invoke a specific browser
+
+def pytest_addoption(parser):
+    parser.addoption("--browser_name",action="store", default="chrome") # browser_name is the key that should be written in CLI along with its desired value
+
+
 @pytest.fixture(scope="class")
 def setup(request): # Using request arg allow us to send driver as an object to methods calling this fixtures
-    #Give path to the browser driver
-    service_obj = Service("C:/Users/divya/Downloads/chromedriver_win32/chromedriver.exe")
+    Browser = request.config.getoption("browser_name") #Here we assign the I/P by the user into the Browser variable
 
-    #If the browser closes automatically, below is how to sustain
-    chrome_options = Options()
-    chrome_options.add_experimental_option("detach", True)
+    if Browser == "chrome":
+        service_obj = Service("C:/Users/divya/Downloads/chromedriver_win32/chromedriver.exe")
+        #If the browser closes automatically, below is how to sustain
+        chrome_options = Options()
+        chrome_options.add_experimental_option("detach", True)
 
-    #DriverObj is intermediation bw our system and chrome
-    driver = webdriver.Chrome(service=service_obj)
+        #DriverObj is intermediation bw our system and chrome
+        driver = webdriver.Chrome(service=service_obj)
+    elif Browser == "firefox":
+        print("Please add the respective executable path here!!!")
 
     driver.implicitly_wait(5)
     request.cls.driver = driver # sending driver object to a class which calls the fixture
